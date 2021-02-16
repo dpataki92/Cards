34880ec8af1947513648e20250213f3670a97d75
dpataki92 <ptamasdaniel92@gmail.com>
2021-02-16 14:10:35 +0100

    update create_deck method

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,7 +1,12 @@
 defmodule Cards do
 
   def create_deck do
-    ["Ace", "Two", "Three"]
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for suit <- suits do
+      "Is this a map?"
+    end
   end
 
   # data structure passed is immutable!

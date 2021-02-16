580b677c2cbd89ef8d094dc420f5fef47de8ea52
dpataki92 <ptamasdaniel92@gmail.com>
2021-02-16 14:24:49 +0100

    solve nested arrays issue in create_deck

--- lib/cards.ex
+++ lib/cards.ex
@@ -4,9 +4,13 @@ defmodule Cards do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
-    for suit <- suits do
-      "Is this a map?"
+    cards = for value <- values do
+      for suit <- suits do
+        "#{value} of #{suit}"
+      end
     end
+
+    List.flatten(cards)
   end
 
   # data structure passed is immutable!
